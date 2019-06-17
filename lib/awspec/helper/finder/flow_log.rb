2f3a3f636e28a5c7031b890d0f1eaf29b9ae2e32
Chris Pollard <chris@chaobaohouse.com>
2019-06-17 21:02:56 +1200

    adding vpc flow log selector

--- lib/awspec/helper/finder/flow_log.rb
+++ lib/awspec/helper/finder/flow_log.rb
@@ -3,8 +3,15 @@ module Awspec::Helper
     module FlowLog
       def find_flow_log(id)
         res = ec2_client.describe_flow_logs({
-                                         filter: [{ name: 'flow-log-id', values: [id] }]
-                                       })
+                                              filter: [{name: 'flow-log-id', values: [id]}]
+                                            })
+        res.flow_logs.single_resource(id)
+      end
+
+      def select_flow_log_by_vpc_id(vpc_id)
+        res = ec2_client.describe_flow_logs({
+                                              filters: [{name: 'vpc-id', values: [vpc_id]}]
+                                            })
         res.flow_logs.single_resource(id)
       end
     end
